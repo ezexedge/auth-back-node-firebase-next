0a8598843f81644692764c7c3cb169e783bd3432
ezequiel gallardo <ezeedge@gmail.com>
2021-03-14 23:01:46 -0300

    cambios back

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+
 const app = express()
 require('dotenv').config()
 
@@ -9,7 +12,12 @@ mongoose
     .then(()=> console.log('db conectada'))
     .catch((err)=> console.log('error----',err))
 
+app.use(cors())
+
+app.use(morgan('dev'))
+
 const port = process.env.PORT || 5000
+
 
 app.listen( port , ()=> {
     console.log('corriendo')
